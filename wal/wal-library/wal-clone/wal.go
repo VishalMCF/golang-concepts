34d991e5843c81e82dc54e6cbe5fcf5bf088e57a
VishalMCF <70262922+VishalMCF@users.noreply.github.com>
2024-01-02 22:02:12 +0530

    added options

--- wal/wal-library/wal-clone/wal.go
+++ wal/wal-library/wal-clone/wal.go
@@ -1,6 +1,9 @@
-package main
+package wal_clone
 
-import "errors"
+import (
+	"errors"
+	"os"
+)
 
 // defining all the errors possible
 
@@ -33,3 +36,41 @@ const (
 	Binary Logformat = 0
 	JSON   Logformat = 1
 )
+
+// Options for log
+type Options struct {
+
+	// if this is true fsync will not happen. What it means is that the written log file/segment might b stored
+	// in the os cache. If this is set to true os will ot flush the changes to the disk and we may lose data if the
+	// system goes down
+	NoSync bool
+
+	// the log is divided into multiple files. Each file is called a segment. The default segment size is 20MB
+	// please search on chatgpt -> why do we need to break the log file into multiple segment
+	SegmentSize int
+
+	// whether to write the data into binary format or json format. json format is readable but the file is typically
+	// larger as compared to the binary counterpart
+	format Logformat
+
+	// segments are also cached in memory to avoid disk look ups. By default at max two segment will be cached
+	SegmentCacheSize int
+
+	// ??-:-> stills needs to be check why it is being used
+	NoCopy bool
+
+	// Perms represents the datafiles modes and permission bits
+	DirPerms  os.FileMode
+	FilePerms os.FileMode
+}
+
+// DefaultOptions for Open().
+var DefaultOptions = &Options{
+	NoSync:           false,    // Fsync after every write
+	SegmentSize:      20971520, // 20 MB log segment files.
+	format:           Binary,   // Binary format is small and fast.
+	SegmentCacheSize: 2,        // Number of cached in-memory segments
+	NoCopy:           false,    // Make a new copy of data for every Read call.
+	DirPerms:         0750,     // Permissions for the created directories
+	FilePerms:        0640,     // Permissions for the created data files
+}
